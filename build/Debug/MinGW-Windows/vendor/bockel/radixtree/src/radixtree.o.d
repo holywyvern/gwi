84b9d2f5389417e22de0e0212e494a9d73727e31
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2015-10-13 12:33:39 -0300

    removed binaries and added gitignore

--- build/Debug/MinGW-Windows/vendor/bockel/radixtree/src/radixtree.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/MinGW-Windows/vendor/bockel/radixtree/src/radixtree.o: \
- vendor/bockel/radixtree/src/radixtree.c \
- vendor/bockel/radixtree/src/radixtree.h
-
-vendor/bockel/radixtree/src/radixtree.h:
